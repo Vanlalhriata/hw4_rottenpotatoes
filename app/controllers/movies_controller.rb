c398828ba9e0db5ff0cf20169e662d7e1c7a2297
saasbook <saasbook@saasbook.(none)>
2012-11-04 17:51:53 -0800

    Homework 4 Part 3.a.2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -64,4 +64,9 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def similar
+    director = Movie.find(params[:id]).director
+    @movies = Movie.find_all_by_director(director)
+  end
+
 end
